679bd51ddbd13fb61c8755c94797cc11ca5d3f0c
PedroEliasCS <peedro_eliias@hotmail.com>
2020-11-05 15:40:29 -0300

    05/11/2020

--- controllers/redireciona.js
+++ controllers/redireciona.js
@@ -3,6 +3,7 @@ let controller = {}
 const fs = require('fs')
 
 
+
 controller.ultimoDia = async (req, res) => {
     await fs.readFile('./ultimoDia.json', 'utf8', (err, data) => {
         if (err) {
@@ -15,64 +16,148 @@ controller.ultimoDia = async (req, res) => {
     })
 }
 
-let cont1 = 0
-const copia = async (dir) => {
-    try {
-        let ext = dir.split('.').pop()
-        let destino = '/teste/destino'
-        let nomeArq = dir.split('/').pop()
-        let nomeDestino = (Date.now() * dir.length) +  nomeArq
-        await fs.copyFile(dir, `${destino}/${nomeDestino}.${ext}`, "utf8", (err) => {
-            if (err) {
-                console.log('erro na copia do arquivo : ', dir)
-                console.log(err)
-            } else {
 
-                // console.log('ok : ' ,dir)
+const deleteDest = async (dir) => { // deleta 
+    try {
+
+        fs.readdir(dir, (err, path) => { // lista todos os nome dos arquivos
+            for (let prop in path) { // passa dentro da lista arquivo por arquivo
+                fs.unlink(dir + path[prop], (err) => { // deleta arquivo por arquivo
+                    if (err) console.log(err) // caso algo de errado
+                })
             }
         })
     } catch (e) {
-        console.log(e, ' erro')
+        logErr(e)
+        return
     }
 }
 
-const stat = async (dir) => {
-    
-    fs.stat(dir, (err, file) => {
-        if (file.isDirectory() === true) {
-            readdir(dir)
-
-        } else if (file.isFile() === true) {
-            cont1++
-            console.log(cont1)
-            copia(dir)
+const anoProcessadoF = () => { // faz o processamento do ano atual
+    fs.readFile('./ultimoDia.json', 'utf8', (err, data) => { // faz a leitura do json
+        if (err) { // caso algo da errado
+            console.log(err)
+        } else { // caso de certo
+            let json = JSON.parse(data) // converta o data Json para JSON 
+            let anoProcessado = json.data // acha a data atual
+            anoProcessado = (new Date(anoProcessado)).getFullYear() // separa o ano processado
+            //console.log(anoProcessado)
+            let dataAtual = new Date() // data atual
+            let anoAtual = dataAtual.getFullYear() // só o ano atual 
+            if (anoProcessado != anoAtual) { // se anos diferirem
+                json.data = dataAtual // troca a data atual do json 
+                json = JSON.stringify(json) // converte em json a data atual
+                fs.writeFile('./ultimoDia.json', json, 'utf8', (err) => { //  sobreescreve arquivo json
+                    if (err) { // caso algo de errado
+                        logErr(err) // chama o log erro
+                        return
+                    } else { // deu certo?
+                        deleteDest('../destino/') // chama a function que deleta 
+                    }
+                })
+            } // else {} caso  o ano seja igual não fazer nada
         }
     })
+}
+
+anoProcessadoF()
+
+const logErr = (e) => {
+    console.log(e)
+    return
+}
+
+const random = () => Math.floor(Math.random() * 10 + 1)
+
+const copia = async (dir) => { // copia o arquivo
+    try {
+
+        let destino = '../destino'
+        // isso aqui tem de vir do usuario
+
+
+        let ext = dir.split('.').pop() // separa a extenção
+        let nomeArq = dir.split('/').pop() // separa somente o nome do arquivo
+        let nomeDestino = (nomeArq.split('.')) + (Date.now() * random()) // cria um nome do arquivo
+      //  console.log(dir)
+        fs.copyFile(dir, `${destino}/${nomeDestino}.${ext}`, "utf8", (err) => {
+            if (err) {
+                logErr(err)
+            }
+        })
+
+    } catch (e) {
+        logErr(e)
+        return
+    }
+}
+
+const mesIndexOf = (ano, dir) => {
+    if((dir.indexOf(`/${ano}/`)) != '-1' || dir.indexOf(`/${ano-1}/12/`) != '-1'){
+        return true
+    }
+}
+
+const filtros = async (dir) => {
+    let ano = (new Date()).getFullYear() 
+    if(dir.indexOf('/Folha') != '-1' || dir.indexOf('/FOLHA')){
+        if(mesIndexOf(ano, dir) === true) {
+            copia(dir)
+        }
+    } 
+}
+const stat = async (dir) => { // um unico arquivo chega nesse passo 
+    try {
+
+        fs.stat(dir, (err, file) => { // abre a descrição do arquivo
+            if (err) {
+                logErr(err)
+                return
+            } {
+                if (file.isDirectory() === true) {
+                    readdir(dir)
+                    //console.log('pasta')
+                } else if ((file.isFile()) == true) {
+                    //console.log('arquivo')
+                    filtros(dir)
+                }
+            }
+        })
+    } catch (e) {
+        logErr(e)
+    }
 }
 
 let cont = 0
-const readdir = async (dir) => {
-    await fs.readdir(dir, (err, path) => {
-        for (let prop in path) {
-            stat((dir + '/' + path[prop]))
-        }
-    })
+const readdir = async (dir) => { // lista os nomes dos arquivos das pasta 
+    try {
+        await fs.readdir(dir, (err, path) => { // lista os nomes dos arquivos da pasta
+            for (let prop in path) {
+                // chama a function Stat para cada arquivo 
+                // de forma asincrona 
+                stat((dir + '/' + path[prop]))
+            }
+        })
 
+    } catch (e) {
+        logErr(e)
+        return
+    }
 }
 
-const deleteDest = async (dir) => {
-    fs.readdir(dir, (err, path) => {
-        for(let prop in path){
-            fs.unlink(dir + path[prop], (err) => {
-                if(err) console.log(err)
-            })
-        }
-    })
-}
 
-controller.redireciona = async () => {
-    let a = await deleteDest('/teste/destino/')
-    let b = await readdir('/teste/aSerem')
+
+
+controller.redireciona = async () => { // controler que fara uma bela bagunça 
+    deleteDest('../destino/on')
+    setTimeout(() => {
+
+        readdir('../Arquivos') // faz a varedura e copia dos arquivos
+    }, 1000);
+
+
+
+
 
 }
 
